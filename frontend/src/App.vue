6b68862218111fceddf2ba289bf0bf5917506d95
sergeshaw <sergeshaw@gmail.com>
2019-09-03 00:22:18 +0200

    add Training Dummy page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
-      {{" | "}}
-      <router-link to="/about">About</router-link>
-      {{" | "}}
       <router-link to="/upload">Upload</router-link>
       {{" | "}}
       <router-link to="/logs">Logs</router-link>
+      {{" | "}}
+      <router-link to="/">Training Dummy</router-link>
     </div>
     <router-view />
   </div>
--- frontend/src/views/TrainingDummy.vue
+++ frontend/src/views/TrainingDummy.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="upload">
-    <h1>Upload page</h1>
+  <div>
+    <h1>Training Dummy page</h1>
     <el-row :gutter="20">
       <el-col :span="10" :offset="7">
         <div>
@@ -10,11 +10,10 @@
           <el-input v-model="location" placeholder="location"></el-input>
         </div>
 
-        <div>
+        <div style="margin-top: 15px;">
           <h2>Select an log file</h2>
           <el-upload :on-remove="removeFile" :on-change="onFileChange" :auto-upload="false">
             <el-button slot="trigger" size="small" type="primary">Choose log file</el-button>
-            <el-button v-show="file" size="small" type="success" @click="submitFile">Upload</el-button>
           </el-upload>
         </div>
       </el-col>
@@ -23,31 +22,34 @@
 </template>
 
 <script>
+import LogParser from "../logParser";
+import POWER_NAMES from "../powerNames.json";
+
 export default {
   name: "Upload",
   data() {
     return {
       file: null,
       username: "",
-      location: ""
+      location: "",
+      logs: []
     };
   },
   methods: {
-    onFileChange(file, fileList) {
-      this.file = file.raw;
-    },
-    async submitFile() {
-      const form = new FormData();
-      form.append("file", this.file);
-      form.append("username", this.username);
-      form.append("location", this.location);
-
-      const res = await fetch("/uploadLog", {
-        method: "POST",
-        body: form
+    async onFileChange(file, fileList) {
+      this.file = await file.raw.text();
+      this.logs = this.file.split("\n").map(log => {
+        const logParser = new LogParser(
+          log,
+          this.location,
+          this.username,
+          POWER_NAMES
+        );
+        logParser.parse();
+        return logParser.getDBData();
       });
-      const data = await res.json();
-      console.log(data);
+
+      console.log(this.logs);
     },
     removeFile() {
       this.file = null;
