5ceb13f367a504b706ad42517d92b1ff43fe940b
Christian Wirth <wirth@protectivecircle.com>
2019-09-06 02:15:48 +0200

    adds first working setup on Fargate

--- init-ecs-cli.sh
+++ init-ecs-cli.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 AWS_ACCESS_KEY_ID=pleaseEnter
 AWS_SECRET_ACCESS_KEY=pleaseEnter
-ecs-cli configure profile --profile-name combat_log --access-key $AWS_ACCESS_KEY_ID --secret-key $AWS_SECRET_ACCESS_KEY
-ecs-cli configure --cluster combat_log --region eu-central-1 --default-launch-type FARGATE --config-name combat_log
+ecs-cli configure profile --profile-name combatLog --access-key $AWS_ACCESS_KEY_ID --secret-key $AWS_SECRET_ACCESS_KEY
+ecs-cli configure --cluster combatLog --region eu-central-1 --default-launch-type FARGATE --config-name combatLog
 aws iam --region eu-central-1 create-role --role-name ecsTaskExecutionRole --assume-role-policy-document file://task-execution-assume-role.json
 aws iam --region eu-central-1 attach-role-policy --role-name ecsTaskExecutionRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy
 ecs-cli up --cluster combatLog
