0c1eeb8038ce934c1a4cf3ddb282c1451ac00658
sergeshaw <sergeshaw@gmail.com>
2019-09-11 13:08:42 +0200

    add skillBy chart refactoring

--- frontend/src/components/DCTest.vue
+++ frontend/src/components/DCTest.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <el-row :gutter="20">
+      <div id="damage-taken-chart" />
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="22" :offset="1">
+        <div id="skill-by-chart" />
+      </el-col>
+    </el-row>
     <el-row :gutter="0">
       <el-col :span="11">
         <div id="skill-name-chart" />
@@ -34,99 +42,198 @@
 .dc-data-count {
   float: none;
 }
+.dc-chart g.row text {
+  fill: #000;
+}
+.x .tick text {
+  transform: translate(-10px, 10px) rotate(-55deg);
+}
 </style>
 
 <script>
-import { scaleLinear } from "d3-scale";
+import { scaleOrdinal } from "d3-scale";
 import "dc/dc.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import dc from "dc";
 import crossfilter from "crossfilter2";
 import dcTestData from "../assets/dc-test-data.json";
 
-dcTestData
-  .filter(a => a.skillName.trim())
-  .forEach(d => {
-    d.dateTime = new Date(d.dateTime);
-    d.date = d.dateTime.toLocaleDateString();
-  });
+const DATA = dcTestData
+  .filter(
+    a =>
+      a.skillName &&
+      a.skillBy &&
+      a.skillTarget &&
+      a.skillName.trim() &&
+      a.skillBy.trim() &&
+      a.skillTarget.trim()
+  )
+  .map(d => ({
+    ...d,
+    dateTime: new Date(d.dateTime)
+  }));
 
 export default {
-  mounted() {
-    const criticalChart = dc.pieChart("#critical-chart");
-    const skillActionChart = dc.pieChart("#skill-action-chart");
-    const skillNameChart = dc.rowChart("#skill-name-chart");
-    const logsCount = dc.dataCount(".dc-data-count");
-    const logsTable = dc.dataTable("#data-table");
+  data() {
+    return {
+      ndx: crossfilter(DATA),
+      username: DATA[0].username
+    };
+  },
+  methods: {
+    prepareCriticalChart() {
+      const criticalChart = dc.pieChart("#critical-chart");
+      const criticalDimension = this.ndx.dimension(d => d.skillCritical);
 
-    const ndx = crossfilter(dcTestData);
-    const all = ndx.groupAll();
+      criticalChart
+        .width(180)
+        .height(180)
+        .radius(90)
+        .dimension(criticalDimension)
+        .group(criticalDimension.group())
+        .label(d => (d.key ? "Critical" : "Uncritical"))
+        .renderLabel(true)
+        .innerRadius(30)
+        .transitionDuration(500);
+    },
+    prepareSkillActionChart() {
+      const skillActionChart = dc.pieChart("#skill-action-chart");
+      const skillActionDimension = this.ndx.dimension(d => d.skillAction);
+      skillActionChart
+        .width(180)
+        .height(180)
+        .radius(90)
+        .dimension(skillActionDimension)
+        .group(skillActionDimension.group())
+        .label(d => d.key)
+        .renderLabel(true)
+        .innerRadius(30)
+        .transitionDuration(500);
+    },
+    prepareSkillsChart() {
+      const skillNameChart = dc.rowChart("#skill-name-chart");
+      const skillNameDimension = this.ndx.dimension(d => d.skillName);
 
-    const dateDimension = ndx.dimension(d => d.dateTime);
-    const skillActionDimension = ndx.dimension(d => d.skillAction);
-    const skillActionGroup = skillActionDimension.group();
-    const criticalDimension = ndx.dimension(d => d.skillCritical);
-    const criticalGroup = criticalDimension.group();
+      const hitPointsPerSkill = skillNameDimension
+        .group()
+        .reduce(
+          (p, v) => p + v.skillAmount,
+          (p, v) => p - v.skillAmount,
+          () => 0
+        );
 
-    const skillNameDimension = ndx.dimension(d => d.skillName);
-    const skillNameGroup = skillNameDimension.group();
+      skillNameChart
+        .cap(25)
+        .height((a, b) => {
+          const count = skillNameDimension.group().size() + 1;
+          return (count > 25 ? 25 : count) * 40;
+        })
+        .dimension(skillNameDimension)
+        .group(hitPointsPerSkill)
+        .elasticX(true);
+    },
+    prepareDamageTakenChart() {
+      const skillByChart = dc.rowChart("#damage-taken-chart");
+      const damageTakenDimension = this.ndx.dimension(d => d.skillBy);
 
-    logsCount.crossfilter(ndx).groupAll(all);
+      const damageTakenPerTarget = damageTakenDimension
+        .filter(d => d !== this.username)
+        .group()
+        .reduce(
+          (p, v) =>
+            v.skillBy === this.username || v.skillTarget !== this.username
+              ? p
+              : p + v.skillAmount,
+          (p, v) =>
+            v.skillBy === this.username || v.skillTarget !== this.username
+              ? p
+              : p - v.skillAmount,
+          () => 0
+        );
+      skillByChart
+        .cap(30)
+        .height((a, b) => {
+          const count = damageTakenPerTarget.size() + 1;
+          return (count > 30 ? 30 : count) * 30;
+        })
+        .dimension(damageTakenDimension)
+        .group(damageTakenPerTarget)
+        .elasticX(true);
+    },
+    prepareSkillByChart() {
+      const skillByChart = dc.barChart("#skill-by-chart");
+      const skillByDimension = this.ndx.dimension(d => d.skillBy);
 
-    logsTable
-      .dimension(dateDimension)
-      .size(15)
-      .columns([
-        "date",
-        "skillAction",
-        "skillName",
-        "skillBy",
-        "skillTarget",
-        "skillAmount",
-        "skillCritical"
-      ]);
+      function sel_stack(i) {
+        return function(d) {
+          return d.value[i];
+        };
+      }
 
-    const hitPointsPerSkill = skillNameDimension
-      .group()
-      .reduce(
-        (p, v) => p + v.skillAmount,
-        (p, v) => p - v.skillAmount,
-        () => 0
+      const hpDealtPerTarget = skillByDimension.group().reduce(
+        (p, v) => {
+          p[v.skillAction] += v.skillAmount;
+          return p;
+        },
+        (p, v) => {
+          p[v.skillAction] -= v.skillAmount;
+          return p;
+        },
+        () => ({ HIT: 0, HEAL: 0 })
       );
 
-    skillNameChart
-      .cap(25)
-      .height((a, b) => {
-        const count = skillNameGroup.size() + 1;
-        return (count > 25 ? 25 : count) * 40;
-      })
-      .dimension(skillNameDimension)
-      .group(hitPointsPerSkill)
-      .elasticX(true);
+      skillByChart
+        .height(400)
+        .margins({ left: 80, top: 20, right: 10, bottom: 50 })
+        .clipPadding(10)
+        .dimension(skillByDimension)
+        .group(hpDealtPerTarget, "Hit", sel_stack("HIT"))
+        .brushOn(false)
+        .xUnits(dc.units.ordinal)
+        .x(scaleOrdinal().domain(skillByDimension))
+        .elasticY(true)
+        .ordering(d => -d.value.HIT - d.value.HEAL);
 
-    skillActionChart
-      .width(180)
-      .height(180)
-      .radius(90)
-      .dimension(skillActionDimension)
-      .group(skillActionGroup)
-      .label(d => d.key)
-      .renderLabel(true)
-      .innerRadius(30)
-      .transitionDuration(500);
+      skillByChart.stack(hpDealtPerTarget, "Heal", sel_stack("HEAL"));
+    },
+    prepareCounter() {
+      const logsCount = dc.dataCount(".dc-data-count");
+      logsCount.crossfilter(this.ndx).groupAll(this.ndx.groupAll());
+    },
+    prepareTabel() {
+      const logsTable = dc.dataTable("#data-table");
+      const dateDimension = this.ndx.dimension(d => d.dateTime);
+      logsTable
+        .dimension(dateDimension)
+        .size(15)
+        .columns([
+          { label: "Date", format: d => d.dateTime.toLocaleDateString() },
+          { label: "Skill Action", format: d => d.skillAction },
+          { label: "Skill Name", format: d => d.skillName },
+          { label: "Skill By", format: d => d.skillBy },
+          { label: "Skill Target", format: d => d.skillTarget },
+          { label: "Skill Amount", format: d => d.skillAmount },
+          {
+            label: "Is Critical",
+            format: d => (d.skillCritical ? "Yes" : "No")
+          }
+        ]);
+    },
 
-    criticalChart
-      .width(180)
-      .height(180)
-      .radius(90)
-      .dimension(criticalDimension)
-      .group(criticalGroup)
-      .label(d => (d.key ? "Critical" : "Uncritical"))
-      .renderLabel(true)
-      .innerRadius(30)
-      .transitionDuration(500);
+    initializeCharts() {
+      this.prepareCriticalChart();
+      this.prepareSkillActionChart();
+      this.prepareSkillsChart();
+      // this.prepareDamageTakenChart();
+      this.prepareSkillByChart();
+      this.prepareCounter();
+      this.prepareTabel();
 
-    dc.renderAll();
+      dc.renderAll();
+    }
+  },
+  mounted() {
+    this.initializeCharts();
   }
 };
 </script>
