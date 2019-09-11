344c4bf6691a02ec2d20a7c378764410ee565039
sergeshaw <sergeshaw@gmail.com>
2019-09-11 14:12:20 +0200

    fix backend

--- backend/fight.mjs
+++ backend/fight.mjs
@@ -43,7 +43,6 @@ export default class Fight {
         l.skillAmount === log.skillAmount
     );
     if (!duplicatedLog) {
-      duplicatedLog.syncronized = true;
       return;
     }
 
--- backend/mongo.mjs
+++ backend/mongo.mjs
@@ -4,32 +4,30 @@ mongoose.connect("mongodb://root:example@mongo:27017/admin", {
   useNewUrlParser: true
 });
 
-export const CombatLog = mongoose.model(
-  "CombatLog",
-  mongoose.Schema({
-    // user_id bigint,
-    // skill_id bigint,
-    // poi_id bigint
+const CombatLogSchema = mongoose.Schema({
+  // user_id bigint,
+  // skill_id bigint,
+  // poi_id bigint
 
-    // TODO: tmp fields, need to be replaced by links or structure
-    username: String,
+  // TODO: tmp fields, need to be replaced by links or structure
+  username: String,
 
-    logId: mongoose.Schema.Types.ObjectId,
-    skillAction: String,
-    skillName: String,
-    dateTime: Date,
-    skillBy: String,
-    skillTarget: String,
-    skillAmount: Number,
-    skillCritical: Boolean,
-    syncronized: Boolean
-  })
-);
+  logId: mongoose.Schema.Types.ObjectId,
+  skillAction: String,
+  skillName: String,
+  dateTime: Date,
+  skillBy: String,
+  skillTarget: String,
+  skillAmount: Number,
+  skillCritical: Boolean
+});
+
+export const CombatLog = mongoose.model("CombatLog", CombatLogSchema);
 
 export const Fight = mongoose.model(
   "Fight",
   mongoose.Schema({
-    logs: [CombatLog],
+    logs: [CombatLogSchema],
     location: {
       campaign: String,
       zone: String,
