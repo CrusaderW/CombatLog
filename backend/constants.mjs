2d35fa5d3514bb2f8301daeb8f36930e6dc29a44
Christian Wirth <wirth@protectivecircle.com>
2019-09-16 10:24:21 +0200

    fixes an issue where skillBy was not set properly

--- backend/constants.mjs
+++ backend/constants.mjs
@@ -10,7 +10,7 @@ export const ACTION_TYPES_PARSING = {
   [ACTION_TYPES.DRAIN_DODGE]: " drained "
 };
 
-export const SKILL_BY_ME = "Your ";
+export const SKILL_BY_ME = "Your";
 export const SKILL_TARGET_ME = " You ";
 export const FOR_SPLITTER = " for ";
 export const EVENT_SPLITTER = "Event=[";
